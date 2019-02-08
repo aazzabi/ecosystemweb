99cb03d1f45a0d76ec9d1f4a2247028c7a7df5b8
Arafet Azzabi <arafet.azzabi@esprit.tn>
2019-02-08 14:03:05 +0100

    fix menu errors

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,13 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/dashboard.html.twig');
+    }
 }

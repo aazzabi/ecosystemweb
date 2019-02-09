8a0f8f6e6fca360a69298e91ab7d5d234ec9522f
Arafet Azzabi <arafet.azzabi@esprit.tn>
2019-02-09 15:55:03 +0100

    Adding EcoBundle

---
+++ src/EcoBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EcoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/dashboard.html.twig');
+    }
+
+}
---
+++ src/EcoBundle/EcoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EcoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EcoBundle extends Bundle
+{
+}

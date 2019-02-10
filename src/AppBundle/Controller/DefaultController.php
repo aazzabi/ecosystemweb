208132ab763840be2e3b19f84f7983944f66efa7
Arafet Azzabi <arafet.azzabi@esprit.tn>
2019-02-10 01:02:16 +0100

    checkout first version for AppBundle/DefaultController : useless controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,13 +18,4 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
-
-    /**
-     * @Route("/dashboard", name="dashboard")
-     */
-    public function dashboardAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/dashboard.html.twig');
-    }
 }

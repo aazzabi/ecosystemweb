e0ca893ed067199d358ce296913b47e5dbeafe0f
AlteraGuda <mohamedmehdi.mheni@esprit.tn>
2019-02-20 19:59:17 +0100

    NewGroupFiles

---
+++ src/EcoBundle/Controller/UserController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace EcoBundle\Controller;
+
+use EcoBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * User controller.
+ *
+ * @Route("user")
+ */
+class UserController extends Controller
+{
+    /**
+     * Lists all user entities.
+     *
+     * @Route("/", name="users_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+//        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+//            throw new AccessDeniedException("Vous n'êtes pas autorisés à accéder à cette page!", Response::HTTP_FORBIDDEN);
+//        }
+
+//        $users = $this->get('model_manager.user')->retrieveAllUsers();
+        $users = $this->getDoctrine()->getManager()->getRepository(User::class)->findAll();
+
+        return $this->render('@Eco/User/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    /**
+     * Creates a new user entity.
+     *
+     * @Route("/new", name="users_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+//        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+//            throw new AccessDeniedException("Vous n'êtes pas autorisés à accéder à cette page!", Response::HTTP_FORBIDDEN);
+//        }
+        $user = new User();
+        $form = $this->createForm('EcoBundle\Form\UserType', $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('users_index', array('id' => $user->getId()));
+        }
+
+        return $this->render('@Eco/User/new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a user entity.
+     *
+     * @Route("/{id}", name="users_show")
+     * @Method("GET")
+     */
+    public function showAction(User $user)
+    {
+//        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+//            throw new AccessDeniedException("Vous n'êtes pas autorisés à accéder à cette page!", Response::HTTP_FORBIDDEN);
+//        }
+        $deleteForm = $this->createDeleteForm($user);
+
+        return $this->render('@Eco/User/show.html.twig', array(
+            'user' => $user,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing user entity.
+     *
+     * @Route("/{id}/edit", name="users_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, User $user)
+    {
+//        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+//            throw new AccessDeniedException("Vous n'êtes pas autorisés à accéder à cette page!", Response::HTTP_FORBIDDEN);
+//        }
+        $deleteForm = $this->createDeleteForm($user);
+        $editForm = $this->createForm('EcoBundle\Form\UserType', $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('users_edit', array('id' => $user->getId()));
+        }
+
+        return $this->render('@Eco/User/edit.html.twig', array(
+            'user' => $user,
+            'form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a user entity.
+     *
+     * @Route("/{id}", name="users_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, User $user)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException("Vous n'êtes pas autorisés à accéder à cette page!", Response::HTTP_FORBIDDEN);
+        }
+        $form = $this->createDeleteForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($user);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('users_index');
+    }
+
+    /**
+     * Creates a form to delete a user entity.
+     *
+     * @param User $user The user entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(User $user)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('users_delete', array('id' => $user->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}

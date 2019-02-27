acd8a701a8bd521d40a566de4e94a413016368ff
bahr anas <anas.bahri@esprit.tn>
2019-02-27 23:15:56 +0100

    fix

--- src/EcoBundle/Controller/Front/FAnnonceController.php
+++ src/EcoBundle/Controller/Front/FAnnonceController.php
@@ -84,18 +84,9 @@ class FAnnonceController extends Controller
         $categories = $em->getRepository('EcoBundle:CategorieAnnonce')->findAll();
         $annnonce = new Annonce();
         $annnonce = $em->getRepository('EcoBundle:Annonce')->findByCategorie($cat);
-        /**
-         * @var $paginator \Knp\Component\Pager\Paginator
-         */
-        $paginator    = $this->get('knp_paginator');
-        $anonnce = $paginator->paginate(
-            $annnonce,
-            $request->query->getInt('page', 1),
-            $request->query->getInt('limit', 9)
-        );
 
         return $this->render('@Eco/Front/Annonce/annonce.html.twig', array(
-            "annonces" => $anonnce, 'categories' => $categories,
+            "annonces" => $annnonce, 'categories' => $categories,
 
         ));
     }
@@ -134,31 +125,11 @@ class FAnnonceController extends Controller
             $categories = $em->getRepository('EcoBundle:CategorieAnnonce')->findAll();
             $annonces = $em->getRepository('EcoBundle:Annonce')->trierPlusRecent();
             $likes = $em->getRepository('EcoBundle:Annonce')->likeAnnonce();
-            /**
-             * @var $paginator \Knp\Component\Pager\Paginator
-             */
-            $paginator    = $this->get('knp_paginator');
-            $anonnce = $paginator->paginate(
-                $annonces,
-                $request->query->getInt('page', 1),
-                $request->query->getInt('limit', 9)
-            );
-
         } elseif ($val == 'PE') {
             $em = $this->getDoctrine()->getManager();
             $categories = $em->getRepository('EcoBundle:CategorieAnnonce')->findAll();
             $annonces = $em->getRepository('EcoBundle:Annonce')->trierPrixElv();
             $likes = $em->getRepository('EcoBundle:Annonce')->likeAnnonce();
-            /**
-             * @var $paginator \Knp\Component\Pager\Paginator
-             */
-            $paginator    = $this->get('knp_paginator');
-            $anonnce = $paginator->paginate(
-                $annonces,
-                $request->query->getInt('page', 1),
-                $request->query->getInt('limit', 9)
-            );
-
 
         } elseif ($val == 'PB') {
 
@@ -166,19 +137,10 @@ class FAnnonceController extends Controller
             $categories = $em->getRepository('EcoBundle:CategorieAnnonce')->findAll();
             $annonces = $em->getRepository('EcoBundle:Annonce')->trierPrixBas();
             $likes = $em->getRepository('EcoBundle:Annonce')->likeAnnonce();
-            /**
-             * @var $paginator \Knp\Component\Pager\Paginator
-             */
-            $paginator    = $this->get('knp_paginator');
-            $anonnce = $paginator->paginate(
-                $annonces,
-                $request->query->getInt('page', 1),
-                $request->query->getInt('limit', 9)
-            );
 
         }
         return $this->render('@Eco/Front/Annonce/annonce.html.twig', array(
-            "annonces" => $anonnce, 'categories' => $categories, 'likes' => $likes,
+            "annonces" => $annonces, 'categories' => $categories, 'likes' => $likes,
         ));
     }
     /**

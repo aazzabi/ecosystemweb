88137baa9122418f2754b4928a1ce3ed94669b04
AlteraGuda <mohamedmehdi.mheni@esprit.tn>
2019-05-24 01:01:10 +0100

    last add ENTITY & API

--- src/EcoBundle/Entity/Host.php
+++ src/EcoBundle/Entity/Host.php
@@ -22,6 +22,150 @@ class Host
     private $id;
 
     /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param string $owner
+     */
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDatestart()
+    {
+        return $this->datestart;
+    }
+
+    /**
+     * @param \DateTime $datestart
+     */
+    public function setDatestart($datestart)
+    {
+        $this->datestart = $datestart;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateend()
+    {
+        return $this->dateend;
+    }
+
+    /**
+     * @param \DateTime $dateend
+     */
+    public function setDateend($dateend)
+    {
+        $this->dateend = $dateend;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalplaces()
+    {
+        return $this->totalplaces;
+    }
+
+    /**
+     * @param int $totalplaces
+     */
+    public function setTotalplaces($totalplaces)
+    {
+        $this->totalplaces = $totalplaces;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAvailableplaces()
+    {
+        return $this->availableplaces;
+    }
+
+    /**
+     * @param int $availableplaces
+     */
+    public function setAvailableplaces($availableplaces)
+    {
+        $this->availableplaces = $availableplaces;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocalisation()
+    {
+        return $this->localisation;
+    }
+
+    /**
+     * @param string $localisation
+     */
+    public function setLocalisation($localisation)
+    {
+        $this->localisation = $localisation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParticipants()
+    {
+        return $this->participants;
+    }
+
+    /**
+     * @param string $participants
+     */
+    public function setParticipants($participants)
+    {
+        $this->participants = $participants;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOwnerid()
+    {
+        return $this->ownerid;
+    }
+
+    /**
+     * @param int $ownerid
+     */
+    public function setOwnerid($ownerid)
+    {
+        $this->ownerid = $ownerid;
+    }
+
+    /**
      * @var string
      *
      * @ORM\Column(name="Owner", type="string", length=200, nullable=true)
